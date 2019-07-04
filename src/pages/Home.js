760963031de5db8e1081a4e1a559b91dd71d2f6a
Kyle Craig <craigkj312@gmail.com>
2019-07-04 11:52:31 -0500

    Changing Home styling.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,21 +37,21 @@ class Home extends React.Component {
         const home = (
             isLoading ? <div className='loading'></div> :
             <div className='home'>
-                <div className='home-title-container'>
-                    <div className='home-title-top'> Clan </div>
-                    <div className='home-title-bottom'> Titles </div>
-                </div>
-                <div className='search-container'>
-                    <input className='search-field' 
-                        placeholder='Enter Clan Name' 
-                        onChange={e => {this.setState({clanName: e.target.value})}}
-                        onKeyPress={e => {
-                            if (e.key === 'Enter') {
-                                this.submitName()
-                            }
-                        }}
-                    />
-                    <div className='press-enter'> Press Enter </div>
+                <div className='home-content'>
+                    <div className='home-title'> Destiny 2 Clan Titles </div>
+                    <div className='home-subtitle'> Compete with your clanmates for activity based titles. </div>
+                    <div className='search-container'>
+                        <input className='search-field' 
+                            placeholder='Enter Clan Name' 
+                            onChange={e => {this.setState({clanName: e.target.value})}}
+                            onKeyPress={e => {
+                                if (e.key === 'Enter') {
+                                    this.submitName()
+                                }
+                            }}
+                        />
+                        <div className='press-enter'> Press Enter to Search </div>
+                    </div>
                 </div>
             </div>
         );
