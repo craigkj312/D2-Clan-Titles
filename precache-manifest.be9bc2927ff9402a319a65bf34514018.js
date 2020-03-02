663cd9e0bb82d6552de44d0e5d06f5837ca4630a
Kyle Craig <craigkj312@gmail.com>
2020-03-01 19:46:46 -0500

    Updates

---
+++ precache-manifest.be9bc2927ff9402a319a65bf34514018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f644f8cec4e4e1c94c5c641dc27d04",
+    "url": "/D2-Clan-Titles/static/media/Traveler-min.72f644f8.jpg"
+  },
+  {
+    "revision": "df54e1dba479c489466a",
+    "url": "/D2-Clan-Titles/static/js/runtime~main.a80cce6e.js"
+  },
+  {
+    "revision": "827c528b43c88c75e81c",
+    "url": "/D2-Clan-Titles/static/js/main.cc01b8ef.chunk.js"
+  },
+  {
+    "revision": "3f89c085a01049edc2be",
+    "url": "/D2-Clan-Titles/static/js/2.401b2d53.chunk.js"
+  },
+  {
+    "revision": "827c528b43c88c75e81c",
+    "url": "/D2-Clan-Titles/static/css/main.cf33c1a0.chunk.css"
+  },
+  {
+    "revision": "a0902fe8900b4bbfc5b14ed8943e5466",
+    "url": "/D2-Clan-Titles/index.html"
+  }
+];
